e3e9d3e0ce43dbfaf97aad9491ffe8cbae1c5bae
yueyehu <hukaijian@aliyun.com>
2020-08-18 17:24:59 +0800

    fix:update quadrotor model

--- Model/Quadrotor/QuadcopterModel.m
+++ Model/Quadrotor/QuadcopterModel.m
@@ -1,4 +1,4 @@
-function QuadcopterModelAda(block)
+function QuadcopterModel(block)
 setup(block);
 
 function setup(block)
@@ -7,28 +7,30 @@ function setup(block)
   %------
   block.NumInputPorts  = 5;
   %------
-  block.NumOutputPorts = 18;
+  block.NumOutputPorts = 6;
   
   % Set up the port properties to be inherited or dynamic.
   
-  for i = 1:5; % These are the motor inputs
+  for i = 1:4 % These are the motor inputs
   block.InputPort(i).Dimensions        = 1;
   block.InputPort(i).DirectFeedthrough = false;
   block.InputPort(i).SamplingMode      = 'Sample';
   end
-  %------
-
-  for i = 1:18;
+  
+  block.InputPort(5).Dimensions        = 6;
+  block.InputPort(5).DirectFeedthrough = false;
+  block.InputPort(5).SamplingMode      = 'Sample';
+  
+  for i = 1:6
   block.OutputPort(i).Dimensions       = 1;
   block.OutputPort(i).SamplingMode     = 'Sample';
   end
 
-
   % Register the parameters.
-  block.NumDialogPrms     = 6;
+  block.NumDialogPrms     = 2;
   
   % Set up the continuous states.
-  block.NumContStates = 18;
+  block.NumContStates = 12;
 
   block.SampleTimes = [0 0];
   
@@ -50,62 +52,62 @@ function setup(block)
 % -------------------------------------------------------------------
 
  function CheckPrms(block)
-     J   = block.DialogPrm(1).Data;
-     R0  = block.DialogPrm(2).Data;
-     m = block.DialogPrm(3).Data;
+ J = block.DialogPrm(1).Data;
+ m = block.DialogPrm(2).Data;
     
 
 function InitializeConditions(block)
-% Initialize 12 States
-
-R0 = block.DialogPrm(2).Data;
-
-for i=1:9
+% Initialize 6 States
+for i=1:6
     block.OutputPort(i).Data = 0;
-    block.ContStates.Data(i) = 0;
 end
-for i=1:3
-    for j=1:3
-        block.OutputPort(6+i*3+j).Data = R0(i,j);
-        block.ContStates.Data(6+i*3+j) = R0(i,j);
-    end
+for i=1:12
+    block.ContStates.Data(i) = 0;
 end
 
 function Outputs(block)
-for i = 1:18;
+for i = 1:3
   block.OutputPort(i).Data = block.ContStates.Data(i);
+end
+for i = 4:6
+  block.OutputPort(i).Data = block.ContStates.Data(i + 3);
 end
 
 function Derivatives(block)
 J = block.DialogPrm(1).Data;
-m = block.DialogPrm(3).Data;
-time = block.InputPort(5).Data;
-if time >1.5
-    m = m*0.8;
-    J = J*[0.9 0 0;0 0.9 0; 0 0 0.8];
-end
+m = block.DialogPrm(2).Data;
 g = 9.81;
-% pos = block.ContStates.Data(1:3);
+
+% get the state of model
 vel = block.ContStates.Data(4:6);
-omega = block.ContStates.Data(7:9);
+euler = block.ContStates.Data(7:9);
+omega = block.ContStates.Data(10:12);
+phi = euler(1); theta = euler(2); psi = euler(3);
+R = [cos(theta)*cos(psi), sin(phi)*sin(theta)*cos(psi) - cos(phi)*sin(psi), cos(phi)*sin(theta)*cos(psi) - sin(phi)*sin(psi);
+     cos(theta)*sin(psi), sin(phi)*sin(theta)*sin(psi) + cos(phi)*cos(psi), cos(phi)*sin(theta)*sin(psi) - sin(phi)*cos(psi);
+     -sin(theta),         sin(phi)*cos(theta),                              cos(phi)*cos(theta)];
+R_oa = [1, sin(phi)*sin(theta)/cos(theta), cos(phi)*sin(theta)/cos(theta);
+        0, cos(phi),                       -sin(phi);
+        0, sin(phi)/cos(theta),            cos(phi)/cos(theta)];
 omega_hat = [0,-omega(3),omega(2);omega(3),0,-omega(1);-omega(2),omega(1),0];
-for i=1:3
-    R(i,:) = block.ContStates.Data(3*i+7:3*(i+3));
+e3 = [0;0;1];
+
+%get input
+F_l = block.InputPort(1).Data;
+for i = 2:4
+    Tau(i - 1) = block.InputPort(i).Data;
 end
 
-f = block.InputPort(1).Data;
-for i=2:4
-    u(i-1) = block.InputPort(i).Data;
-end
+% Get Drag coefficients
+D_v = block.InputPort(5).Data(1:3);
+D_omega = block.InputPort(5).Data(4:6);
+
 d_pos = vel;
-e3=[0;0;1];
-d_vel = (m*g*e3-f*R*e3)/m;
-d_omega = inv(J)*(u.'-omega_hat*J*omega);
-d_R = R*omega_hat;
+d_vel = (m * g * e3 - F_l * R * e3 - D_v .* vel)/m;
+d_euler = R_oa * omega;
+d_omega = inv(J) * (Tau.'-omega_hat * J * omega - D_omega .* omega);
 
-f = [d_pos(1),d_pos(2),d_pos(3),d_vel(1),d_vel(2),d_vel(3),d_omega(1),d_omega(2),d_omega(3),d_R(1,1),d_R(1,2),d_R(1,3),d_R(2,1),d_R(2,2),d_R(2,3),d_R(3,1),d_R(3,2),d_R(3,3)].';
-  %This is the state derivative vector
-block.Derivatives.Data = f;
-
-
+d_state = [d_pos(1),d_pos(2),d_pos(3),d_vel(1),d_vel(2),d_vel(3),d_euler(1),d_euler(2),d_euler(3), d_omega(1),d_omega(2),d_omega(3)].';
+%This is the state derivative vector
+block.Derivatives.Data = d_state;
 %endfunction
